e91ea4638aa952a07d9914f953e02c68b20d7ee7
hectorortizsuarez3 <hectorortizsuarez3@gmail.com>
2025-08-15 13:06:10 +0200

    bootstrap activado

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -118,8 +118,8 @@ document.addEventListener('DOMContentLoaded', function () {
     actualizarContador();
     mensaje.addEventListener('input', actualizarContador);
 
+{{-- Bloque para evitar que el usuario envíe dos veces el formulario por error--}}
     form.addEventListener('submit', function (e) {
-        // Deja que la validación nativa del navegador actúe
         if (!form.checkValidity()) return;
         // Si todo es válido, desactiva el botón para evitar duplicados
         enviarBtn.disabled = true;
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Restaurante Laravel</title>
 </head>
 <body>
