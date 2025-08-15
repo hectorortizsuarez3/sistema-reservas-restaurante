7702251b7fd71b7113d44bcb124693b1fcf02040
hectorortizsuarez3 <hectorortizsuarez3@gmail.com>
2025-08-15 17:02:49 +0200

    envío de emails con cola en sección contacto
    Se activó la opción de envío de emails con cola para el formulario de contacto

--- app/Mail/ContactMessage.php
+++ app/Mail/ContactMessage.php
@@ -5,8 +5,9 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ContactMessage extends Mailable
+class ContactMessage extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
